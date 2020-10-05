72957df20a18eb9b049cf5b8e075c323b90fbd31
Rafael Sacomani <rafael.sacomani@xpi.com.br>
2020-10-05 09:34:29 -0300

    first commit <3

---
+++ src/MonoBook.Implementation/MainBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace MonoBook.Implementation
+{
+    public class MainBook : Game
+    {
+		GraphicsDeviceManager graphics;
+
+        public MainBook()
+        {
+			graphics = new GraphicsDeviceManager(this);
+			graphics.IsFullScreen = true;
+
+		}
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+
+		protected override void LoadContent()
+		{
+		
+
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ src/MonoBook.iOS/AppDelegate.cs
@@ -0,0 +1,20 @@
+﻿using Foundation;
+using MonoBook.Implementation;
+using UIKit;
+
+namespace MonoBook.iOS.iOS
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		MainBook game;
+
+		public override void FinishedLaunching(UIApplication application)
+		{
+			game = new MainBook();
+			game.Run();
+		}
+	}
+}
