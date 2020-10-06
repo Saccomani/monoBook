bcffa03e5362ed96079dfd89e7b498f4eaf0b794
Rafael Sacomani <rafael.sacomani@xpi.com.br>
2020-10-06 09:43:51 -0300

    android proj added

---
+++ src/MonoBook.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Microsoft.Xna.Framework;
+using MonoBook.Implementation;
+
+namespace MonoBook.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AndroidGameActivity
+    {
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			MainBook game;
+
+			base.OnCreate(savedInstanceState);
+			game = new MainBook();
+
+			SetContentView((View)game.Services.GetService(typeof(View)));
+			game.Run();
+
+		}
+	}
+}
