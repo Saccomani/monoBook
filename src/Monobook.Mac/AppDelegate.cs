3ba11c421c4a2c50b59a84267d2e5242cb8fb0cd
Rafael Sacomani <rafael.sacomani@xpi.com.br>
2020-10-06 10:01:58 -0300

    mac proj added

---
+++ src/Monobook.Mac/AppDelegate.cs
@@ -0,0 +1,30 @@
+﻿using AppKit;
+using Foundation;
+using MonoBook.Implementation;
+
+namespace Monobook.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+
+        MainBook game;
+
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+
+            game = new MainBook();
+            game.Run();
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
